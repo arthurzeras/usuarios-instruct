bcac31380c4cc23bbecb349b7c8cd8276a4d01a9
arthurzeras <arthurolvmorais@gmail.com>
2019-08-23 21:37:47 -0300

    Buscando usuários na api

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,30 @@
+Vue.use(VueResource)
+
+Vue.http.options.root = 'http://jsonplaceholder.typicode.com/'
+
 new Vue({
   el: '#app',
   data: {
     filtro: '',
+    usuarios: [],
+    buscandoUsuarios: true,
     filtros: ['.biz', '.org', '.net']
+  },
+  created () {
+    this.buscarUsuarios()
+  },
+  methods: {
+    buscarUsuarios () {
+      this.$http.get('users')
+        .then(res => {
+          this.usuarios = res.data
+        })
+        .catch(erro => {
+          console.error(erro)
+        })
+        .finally(() => {
+          this.buscandoUsuarios = false
+        })
+    }
   }
 })
